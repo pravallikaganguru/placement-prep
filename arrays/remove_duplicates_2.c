752b243db4142894ad153ba4aa3cc18e645e6f76
pravallika <pravallikaganguru@gmail.com>
2026-06-18 20:45:53 +0530

    Day 2 DSA - Majority Element, Rotate Array, Remove Duplicates II

---
+++ arrays/remove_duplicates_2.c
@@ -0,0 +1,19 @@
+int removeDuplicates(int* nums, int numsSize) 
+{
+    if(numsSize <= 2)
+        return numsSize;
+    int k=2,i;
+    for(i=2;i<numsSize;i++)
+    {
+        if(nums[i]!=nums[k-2])
+        {
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+
+
+
+    return k;
+    
+}
